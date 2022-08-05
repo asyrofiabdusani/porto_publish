ff165a37a5ed835648e6b9bcbb213e80d64d2545
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-08-05 21:30:07 +0700

    complete the version -1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const heroImg = document.querySelector('.hero-1');
 const botmeCont = document.querySelector('.icon-botme-container');
 const icon = document.querySelector('.icon');
+const email = document.querySelector('.email').outerText;
+const copyEmailBt = document.querySelector('.copy-button');
 
 document.addEventListener('scroll', function () {
     let position = Math.round(window.scrollY);
@@ -11,4 +13,10 @@ document.addEventListener('scroll', function () {
     if (position < 400) {
         heroImg.style.transform = `translateY(${position - (position / 20)}px)`;
     }
+});
+
+copyEmailBt.addEventListener('click', function () {
+    navigator.clipboard.writeText(email);
+
+    alert("Copied email : " + email);
 });
